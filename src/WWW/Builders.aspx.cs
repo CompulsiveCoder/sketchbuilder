d98c87dfbf644cebdf984e1eb9ab9b7c558a5ed0
CompulsiveCoder <compulsivecoder@gmail.com>
2015-09-27 15:25:18 +1000

    Added missing files

---
+++ src/WWW/Builders.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WWW
+{
+	public partial class Builders : System.Web.UI.Page
+	{
+		public string[] BuilderNames = new string[]{};
+
+		public void Page_Load(object sender, EventArgs e)
+		{
+			var list = new List<string> ();
+
+			foreach (var dir in Directory.GetDirectories(Server.MapPath("builders"))) {
+				list.Add (Path.GetFileNameWithoutExtension (dir));
+			}
+
+			BuilderNames = list.ToArray ();
+		}
+	}
+}
+
