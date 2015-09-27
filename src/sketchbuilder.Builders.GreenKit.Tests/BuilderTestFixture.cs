5ed69a2dc4d30f5ee4d0290c8f7630a0ac13b51e
CompulsiveCoder <compulsivecoder@gmail.com>
2015-09-27 15:44:42 +1000

    Added missing files

---
+++ src/sketchbuilder.Builders.GreenKit.Tests/BuilderTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace sketchbuilder.Builders.GreenKit.Tests
+{
+  [TestFixture]
+  public class BuilderTestFixture
+  {
+    [Test]
+    public void Test_Build()
+    {
+      var src = "/home/j/cloud/primary/Projects/devkit/template";
+
+      var builder = new GreenKitBuilder (src);
+
+      var output = builder.Build (new int[]{ 1, 2 }, new int[]{5,6}, new int[]{4,5});
+
+      Console.WriteLine (output);
+    }
+  }
+}
+
---
+++ src/sketchbuilder.Builders.GreenKit/GreenKitBuilder.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using sketchbuilder.Core;
+
+namespace sketchbuilder.Builders.GreenKit
+{
+  public class GreenKitBuilder
+  {
+    public string SourcePath { get; set; }
+
+    public GreenKitBuilder (string sourcePath)
+    {
+      SourcePath = sourcePath;
+    }
+
+    public string Build(int[] sensorPins, int[] pumpPins, int[] thresholdPins)
+    {
+
+      var templateFileName = Path.Combine (SourcePath, "template.ino");
+
+      var templateCode = File.ReadAllText (templateFileName);
+
+      var newCode = templateCode;
+
+      newCode = InsertSensorPinCount (newCode, sensorPins.Length);
+
+      newCode = InsertSensorPins (newCode, sensorPins);
+
+      newCode = InsertPumpPins (newCode, pumpPins);
+
+      newCode = InsertThresholdPins (newCode, thresholdPins);
+
+      return newCode;
+    }
+
+    public string InsertSensorPinCount(string template, int pinCount)
+    {
+
+      var code = "const int sensorPinCount = " + pinCount + ";";
+
+      var parser = new Parser ();
+
+      return parser.Insert (template, "SensorPinCount", code);
+    }
+
+    public string InsertSensorPins(string template, int[] sensorPins)
+    {
+
+      var codeSnippet = "const int sensorPins[] = {";
+
+      foreach (var pin in sensorPins) {
+        codeSnippet += pin + ",";
+      }
+      codeSnippet = codeSnippet.TrimEnd (',');
+      codeSnippet += "};";
+
+      var parser = new Parser ();
+
+      return parser.Insert (template, "SensorPins", codeSnippet);
+    }
+
+    public string InsertPumpPins(string template, int[] pumpPins)
+    {
+
+      var codeSnippet = "const int pumpPins[] = {";
+
+      foreach (var pin in pumpPins) {
+        codeSnippet += pin + ",";
+      }
+      codeSnippet = codeSnippet.TrimEnd (',');
+      codeSnippet += "};";
+
+      var parser = new Parser ();
+
+      return parser.Insert (template, "PumpPins", codeSnippet);
+    }
+
+
+    public string InsertThresholdPins(string template, int[] thresholdPins)
+    {
+
+      var codeSnippet = "int thresholdPins[] = {";
+
+      foreach (var pin in thresholdPins) {
+        codeSnippet += pin + ",";
+      }
+      codeSnippet = codeSnippet.TrimEnd (',');
+      codeSnippet += "};";
+
+      var parser = new Parser ();
+
+      return parser.Insert (template, "ThresholdPins", codeSnippet);
+    }
+
+    public string ToString(List<char> chars)
+    {
+      var output = String.Empty;
+
+      foreach (var c in chars.ToArray())
+      {
+        output += c.ToString();
+      }
+
+      return output;
+    }
+  }
+}
+
